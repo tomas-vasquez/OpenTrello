7fd243f6d02c624a704adb486748e6da1022cd7a
tomasdetloging <tomasdetloging@gmail.com>
2020-10-21 12:53:36 -0400

    checking

--- src/site.config.js
+++ src/site.config.js
@@ -1,2 +1,7 @@
 const apiUrl = process.env.API_URL || "http://localhost:3001";
+
+console.log("-----------------------------------");
+console.log(process.env);
+
+console.log("-----------------------------------");
 export { apiUrl };
